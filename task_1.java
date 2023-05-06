71af9b535e2b4e6a1b33fb6f90ff545d251514fb
Maksim Voronkin <jh1337@yandex.ru>
2023-05-06 18:57:50 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,7 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+    boolean expected = true;
+	assertEquals("Person's age is equal to or more than 18", expected, isAdult);
+}
